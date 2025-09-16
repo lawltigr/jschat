af229ca41bd17196399e00dc5dc2d5ef13e99b44
lawltigr <vasilisatikhonova110@gmail.com>
2025-09-15 22:48:03 -0700

    changes to css and js

---
+++ chat.js
@@ -0,0 +1,4 @@
+const chatEl = document.getElementById('chat');
+const formEl = document.getElementById('form');
+const inputEl = document.getElementById('input');
+const clearBtn = document.getElementById('clearBtn');
